0b3025e3997b7818d2bbf7c6e00848e25bd18571
PenkiJenki <51465692+PenkiJenki@users.noreply.github.com>
2021-04-04 17:36:51 -0300

    Design inicial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+  constructor() {
 
-  constructor() {}
+    var Pokedex = require('pokedex-promise-v2');
+
+    var options = {
+      protocol: 'https',
+      hostName: 'pokeapi.co',
+      versionPath: '/api/v2/',
+      cacheLimit: 100 * 1000, // 100s
+      timeout: 5 * 1000 // 5s
+    }
+  
+    var P = new Pokedex(options);
+
+    P.getPokemonByName('eevee') // with Promise
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log('There was an ERROR: ', error);
+    });
+
+  }
+
+
 
 }
