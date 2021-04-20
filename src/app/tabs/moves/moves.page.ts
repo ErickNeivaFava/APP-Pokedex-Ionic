c4873095f0a4bd9809cb1ece033279c489c4fef7
Math-rm1 <matheus.r.mariano@hotmail.com>
2021-04-19 21:10:24 -0300

    Bug na filtragem da pagina moves corrigido

--- src/app/tabs/moves/moves.page.ts
+++ src/app/tabs/moves/moves.page.ts
@@ -31,10 +31,10 @@ export class MovesPage implements OnInit {
 
   public moves: Move[] = [
     { id: 1, power: 90, accuracy: 75, category: 'physical', type: 'normal', name: 'pound' },
-    { id: 2, power: 90, accuracy: 75, category: 'physical', type: 'fighting', name: 'karate Chop' },
-    { id: 3, power: 90, accuracy: 75, category: 'physical', type: 'flying', name: 'wing Attack' },
+    { id: 2, power: 90, accuracy: 75, category: 'physical', type: 'fighting', name: 'karate chop' },
+    { id: 3, power: 90, accuracy: 75, category: 'physical', type: 'flying', name: 'wing attack' },
     { id: 4, power: 90, accuracy: 75, category: 'special', type: 'fire', name: 'ember' },
-    { id: 5, power: 90, accuracy: 75, category: 'special', type: 'ice', name: 'aurora Beam' },
+    { id: 5, power: 90, accuracy: 75, category: 'special', type: 'ice', name: 'aurora beam' },
     { id: 6, power: 90, accuracy: 75, category: 'special', type: 'electric', name: 'thunderbolt' },
     { id: 7, power: 90, accuracy: 75, category: 'physical', type: 'dark', name: 'bite' },
     { id: 8, power: 90, accuracy: 75, category: 'special', type: 'electric', name: 'thunder' },
