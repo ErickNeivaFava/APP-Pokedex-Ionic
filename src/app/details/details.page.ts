3fce5609927e243fb3d00334713a7a0e5075c9b1
PenkiJenki <51465692+PenkiJenki@users.noreply.github.com>
2021-06-13 20:00:31 -0300

    Evoluções utilizando services e routerLink

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,13 +1,12 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FavoriteService } from '../services/favorite.service';
 import { LoginService } from '../services/login.service';
 import { MovesService } from '../services/moves.service';
 import { PokedexService } from '../services/pokedex.service';
+import { PokemonService } from '../services/pokemon.service';
 import { EvoChain } from '../types/evoChain.type';
 import { PokemonMove, Pokemon, PokemonAbility } from '../types/pokemon.type';
-import { PokeSpecies } from '../types/pokeSpecies.type';
 
 @Component({
   selector: 'app-details',
@@ -20,6 +19,7 @@ export class DetailsPage implements OnInit {
   public isFavorite: boolean;
   public currentPokemon: Pokemon;
   public nameOfUser: string;
+  public evoChain: EvoChain;
 
   ngOnInit() {
     this.id = +this.route.snapshot.paramMap.get('id');
@@ -27,19 +27,19 @@ export class DetailsPage implements OnInit {
   }
 
   constructor(
+    private pokemonService: PokemonService,
     private route: ActivatedRoute,
     private favoriteService: FavoriteService,
     private pokedexService: PokedexService,
     private movesService: MovesService,
-    private loginService: LoginService,
-    private http: HttpClient
-  ) {}
+    private loginService: LoginService
+  ) { }
 
   public async onLoad(): Promise<void> {
     await this.favoriteService.loadFromStorage();
     this.allFavoritePokemon = this.favoriteService.allFavoritePokemon;
     await this.checkFavorite();
-    this.getPokeEvoChain(this.id);
+    await this.getPokeEvoChain(this.id);
     this.nameOfUser = this.loginService.nameOfUser;
   }
 
@@ -66,53 +66,17 @@ export class DetailsPage implements OnInit {
   public async getPoke(id: number): Promise<void> {
     const response = await this.pokedexService.P.getPokemonByName(id);
     this.currentPokemon = response;
-    // console.log(response.species);
   }
 
   public async getPokeEvoChain(id: number): Promise<void> {
-    const response = await this.pokedexService.P.getPokemonByName(id);
+    this.evoChain = await this.pokemonService.getEvoChain(id);
+  }
 
-    let pokeSpecies = await this.http
-      .get<PokeSpecies>(response.species.url)
-      .toPromise();
+  public getImgUrl(url: string): string {
+    return this.pokemonService.getImg(url);
+  }
 
-    let evoChain = await this.http
-      .get<EvoChain>(pokeSpecies.evolution_chain.url)
-      .toPromise();
-
-    console.log(evoChain); // objeto da chain
-
-    console.log(
-      evoChain.chain.species.name +
-        ' Link para requisição: ' +
-        evoChain.chain.species.url
-    ); // primeiro
-
-    evoChain.chain.evolves_to.forEach(async (element) => {
-      console.log(
-        element.species.name +
-          ' => ' +
-          element.evolution_details[0].trigger.name +
-          '(' +
-          element.evolution_details[0].item +
-          ')' +
-          ' Link para requisição: ' +
-          element.species.url
-      ); // evolução da primeira
-      element.evolves_to.forEach((element2) => {
-        console.log(
-          element2.species.name +
-            ' => ' +
-            element2.evolution_details[0].trigger.name +
-            '(' +
-            element2.evolution_details[0].item +
-            ')' +
-            ' Link para requisição: ' +
-            element2.species.url
-        ); // evolução da segunda
-      });
-    });
-
-    console.log(evoChain.chain.evolves_to.length); // tamanho da chain
+  public getPokeId(url: string): string {
+    return this.pokemonService.getId(url);
   }
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,12 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { EvoChain } from '../types/evoChain.type';
 import { Pokemon } from '../types/pokemon.type';
+import { PokeSpecies } from '../types/pokeSpecies.type';
 import { PokedexService } from './pokedex.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PokemonService {
+  public evoChain: EvoChain;
   public allPokemon: Pokemon[] = [];
 
   public isPokeOnList(pokemon: Pokemon): boolean {
@@ -14,8 +17,8 @@ export class PokemonService {
   }
 
   public async fillPokemonList(results: any): Promise<void> {
-    for (let result of results) {
-      let pokemon = await this.http.get<Pokemon>(result.url).toPromise();
+    for (const result of results) {
+      const pokemon = await this.http.get<Pokemon>(result.url).toPromise();
       !this.isPokeOnList(pokemon) ? this.allPokemon.push(pokemon) : '';
     }
   }
@@ -35,8 +38,31 @@ export class PokemonService {
     }, 500);
   }
 
+  public getImg(url: string): string {
+    const splited = url.split('/').filter((x) => x);
+    const id = splited[splited.length - 1];
+    return `//raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+  }
+
+  public getId(url: string): string {
+    const splited = url.split('/').filter((x) => x);
+    const id = splited[splited.length - 1];
+    return id;
+  }
+
+  public async getEvoChain(id: number): Promise<EvoChain> {
+    const response = await this.pokedexService.P.getPokemonByName(id);
+    const pokeSpecies = await this.http
+      .get<PokeSpecies>(response.species.url)
+      .toPromise();
+    const evoChain = await this.http
+      .get<EvoChain>(pokeSpecies.evolution_chain.url)
+      .toPromise();
+    return evoChain;
+  }
+
   constructor(
     private http: HttpClient,
     private pokedexService: PokedexService
-  ) {}
+  ) { }
 }
